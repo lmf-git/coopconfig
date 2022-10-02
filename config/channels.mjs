11a857ce4fb6ceb246f351d3f2bda2a94d7f513d
L M Fielding <contact@lmfielding.com>
2022-10-02 03:01:34 +0100

    Add member guide channel to config

--- config/channels.mjs
+++ config/channels.mjs
@@ -60,6 +60,10 @@ export default {
     "STOCKS_VC_TEXT": {
         "id": "1009109018716946473",
         "spammable": true
+    },
+
+    "MEMBER_GUIDE": {
+        "id": "https://discord.com/channels/723660447508725802/900739824821096538"
     }
     
 }
