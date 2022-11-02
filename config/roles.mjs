af25ad9b029be875cff99de155685e33b52e1328
L M Fielding <contact@lmfielding.com>
2022-11-02 01:50:24 +0000

    Add role categories

--- config/roles.mjs
+++ config/roles.mjs
@@ -1,82 +1,111 @@
 export default {
     "MEMBER": {
         "name": "member 💛‍",
-        "id": "724394130465357915"
+        "id": "724394130465357915",
+        "category": ""
     },
     "COMMANDER": {
         "name": "👑.  commander",
-        "id": "727311131705868299"
+        "id": "727311131705868299",
+        "category": ""
     },
     "LEADER": {
         "name": "⚔️.  leader",
-        "id": "723676356818239773"
+        "id": "723676356818239773",
+        "category": ""
     },
     "BEGINNER": {
         "name": "beginner 🥚",
-        "id": "725531274973937664"
+        "id": "725531274973937664",
+        "category": ""
     },
     "SUBSCRIBER": {
         "name": "announcements",
-        "id": "726835640804704267"
+        "id": "726835640804704267",
+        "category": "NOTIFICATION"
     },
     "MOSTPOINTS": {
-        "id": "780116637197926420"
+        "id": "780116637197926420",
+        "category": "REWARD"
     },
     "MEMBEROFWEEK": {
-        "id": "725406725615779922"
+        "id": "725406725615779922",
+        "category": "REWARD"
     },
     "LOGS": {
-        "id": "796495195930886155"
+        "id": "796495195930886155",
+        "category": ""
     },
     "FORMER_COMMANDER": {
-        "id": "799113973282111541"
+        "id": "799113973282111541",
+        "category": ""
     },
     "PROJECTS": {
-        "id": "799318707246727188"
+        "id": "799318707246727188",
+        "category": ""
     },
     "CONQUEST": {
-        "id": "799318574077444107"
+        "id": "799318574077444107",
+        "category": ""
     },
     "CODE": {
-        "id": "727512898741600358"
+        "id": "727512898741600358",
+        "category": "INTEREST"
     },
     "BUSINESS": {
-        "id": "725531758744961114"
+        "id": "725531758744961114",
+        "category": "INTEREST"
     },
     "ART": {
-        "id": "727513157374967879"
+        "id": "727513157374967879",
+        "category": "INTEREST"
+    },
+    "FITNESS": {
+        "id": "",
+        "category": "INTEREST"
     },
     "SOCIAL": {
-        "id": "799419324485599242"
+        "id": "799419324485599242",
+        "category": "INTEREST"
     },
     "GAMING": {
-        "id": "811317877151760384"
+        "id": "811317877151760384",
+        "category": "INTEREST"
     },
     "PROSPECT": {
-        "id": "811689644131483648"
+        "id": "811689644131483648",
+        "category": "ELECTION"
     },
-    "AGENCY": {
-        "id": "733739458855174265"
-    },
-    "ACADEMY": {
-        "id": "733739784643674253"
-    },
+    // "AGENCY": {
+    //     "id": "733739458855174265",
+    //     "category": "INTEREST"
+    // },
+    // "ACADEMY": {
+    //     "id": "733739784643674253",
+    //     "category": "INTEREST"
+    // },
     "RICHEST": {
-        "id": "821922924138463232"
+        "id": "821922924138463232",
+        "category": "REWARD"
     },
     "MOST_ITEMS": {
-        "id": "821922830613479424"
+        "id": "821922830613479424",
+        "category": "REWARD"
     },
     "SUPPORTER": {
-        "id": "904477558928715776"
+        "id": "904477558928715776",
+        "category": ""
     },
     "MINIGAME_PING": {
-        "id": "916135701488209940"
+        "id": "916135701488209940",
+        "category": "NOTIFICATION"
     },
     "INTRO_POSTED_PING": {
-        "id": "916116514401316904"
+        "id": "916116514401316904",
+        "category": "NOTIFICATION"
     },
     "MARKET_OPEN_PING": {
-        "id": "1004584945202765844"
+        "id": "1004584945202765844",
+        "category": "MONEY"
     }
 }
