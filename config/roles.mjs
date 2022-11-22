2505fcf54eb6c9619dd18a5e7a43913620220cda
L M Fielding <contact@lmfielding.com>
2022-11-22 03:41:53 +0000

    Categorise roles

--- config/roles.mjs
+++ config/roles.mjs
@@ -1,110 +1,186 @@
 export default {
     "MEMBER": {
-        "name": "member 💛‍",
+        "name": "Member 🐔",
         "id": "724394130465357915",
-        "category": ""
+        "category": "DEMOCRACY"
     },
     "COMMANDER": {
-        "name": "👑.  commander",
+        "name": "Commander 👑",
         "id": "727311131705868299",
-        "category": ""
+        "category": "DEMOCRACY"
     },
     "LEADER": {
-        "name": "⚔️.  leader",
+        "name": "Leader ⚔️",
         "id": "723676356818239773",
-        "category": ""
+        "category": "DEMOCRACY"
     },
+    "PROSPECT": {
+        "name": "Prospect",
+        "id": "811689644131483648",
+        "category": "DEMOCRACY"
+    },
+
     "BEGINNER": {
-        "name": "beginner 🥚",
+        "name": "Beginner 🥚",
         "id": "725531274973937664",
-        "category": ""
+        "category": "REWARD"
     },
     "SUBSCRIBER": {
-        "name": "announcements",
+        "name": "Announcements",
         "id": "726835640804704267",
         "category": "NOTIFICATION"
     },
     "MOSTPOINTS": {
+        "name": "Most Points",
         "id": "780116637197926420",
         "category": "REWARD"
     },
     "MEMBEROFWEEK": {
+        "name": "Member Of Week",
         "id": "725406725615779922",
         "category": "REWARD"
     },
-    "LOGS": {
-        "id": "796495195930886155",
-        "category": ""
-    },
-    "FORMER_COMMANDER": {
-        "id": "799113973282111541",
-        "category": ""
-    },
-    "PROJECTS": {
-        "id": "799318707246727188",
-        "category": ""
-    },
-    "CONQUEST": {
-        "id": "799318574077444107",
-        "category": ""
-    },
-    "CODE": {
-        "id": "727512898741600358",
-        "category": "INTEREST"
-    },
-    "MONEY": {
-        "id": "725531758744961114",
-        "category": "INTEREST"
-    },
-    "ART": {
-        "id": "727513157374967879",
-        "category": "INTEREST"
-    },
-    "SOCIAL": {
-        "id": "799419324485599242",
-        "category": "INTEREST"
-    },
-    "GAMING": {
-        "id": "811317877151760384",
-        "category": "INTEREST"
-    },
-    "PROSPECT": {
-        "id": "811689644131483648",
-        "category": "ELECTION"
-    },
-    // "AGENCY": {
-    //     "id": "733739458855174265",
-    //     "category": "INTEREST"
-    // },
-    // "ACADEMY": {
-    //     "id": "733739784643674253",
-    //     "category": "INTEREST"
+    // "FORMER_COMMANDER": {
+    //     "name": "Former Commander",
+    //     "id": "799113973282111541",
+    //     "category": "REWARD"
     // },
     "RICHEST": {
+        "name": "RICHEST",
         "id": "821922924138463232",
         "category": "REWARD"
     },
     "MOST_ITEMS": {
+        "name": "MOST_ITEMS",
         "id": "821922830613479424",
         "category": "REWARD"
     },
     "SUPPORTER": {
+        "name": "SUPPORTER",
         "id": "904477558928715776",
-        "category": ""
+        "category": "REWARD"
     },
+
+    "PROJECTS": {
+        "name": "Projects",
+        "id": "799318707246727188",
+        "category": "ACCESS"
+    },
+    "SOCIAL": {
+        "name": "Social",
+        "id": "799419324485599242",
+        "category": "ACCESS"
+    },
+    "LOGS": {
+        "name": "Logs",
+        "id": "796495195930886155",
+        "category": "ACCESS"
+    },
+
+    "TECH": {
+        "name": "Tech",
+        "id": "727512898741600358",
+        "category": "INTEREST"
+    },
+    "MONEY": {
+        "name": "Money",
+        "id": "725531758744961114",
+        "category": "INTEREST"
+    },
+    "ART": {
+        "name": "Art",
+        "id": "727513157374967879",
+        "category": "INTEREST"
+    },
+
+
     "MINIGAME_PING": {
+        "name": "MINIGAME_PING",
         "id": "916135701488209940",
         "category": "NOTIFICATION"
     },
     "INTRO_POSTED_PING": {
+        "name": "INTRO_POSTED_PING",
         "id": "916116514401316904",
         "category": "NOTIFICATION"
     },
     "MARKET_OPEN_PING": {
+        "name": "MARKET_OPEN_PING",
         "id": "1004584945202765844",
         "category": "MONEY"
     },
     "NEW_COOP_DAY": {
-        "id": "1029396511789953075"
+        "name": "NEW_COOP_DAY",
+        "id": "1029396511789953075",
+        "category": "NOTIFICATION"
     }
+
+    // "CONQUEST": {
+        // "name": "CONQUEST",
+    //     "id": "799318574077444107",
+    //     "category": ""
+    // },
+    // "GAMING": {
+        // "name": "GAMING",
+    //     "id": "811317877151760384",
+    //     "category": "INTEREST"
+    // },
+    // "AGENCY": {
+        // "name": "AGENCY",
+    //     "id": "733739458855174265",
+    //     "category": "INTEREST"
+    // },
+    // "ACADEMY": {
+        // "name": "ACADEMY",
+    //     "id": "733739784643674253",
+    //     "category": "INTEREST"
+    // },
+
+
+//     <!-- 
+//     Leader 723676356818239773
+//     Supporter 904477558928715776
+//     Former Commander 799113973282111541
+//     Richest 821922924138463232
+//     Tech Event 916478331908669440
+//     Logs 📉 796495195930886155
+
+//     Stocks Early Bird ⏰ 1004584945202765844
+
+//     Announcement 📢 726835640804704267
+//     Stocks Spam Victims 1004588123877023755
+//     Art 🖌️ 727513157374967879
+//     Business Event 916478284437520434
+//     Gaming  🎮 811317877151760384
+//     Social 💬 799419324485599242
+//     Minigame Ping 📦 916135701488209940
+
+//     Democracy Ping ?
+//     Intro Posted Ping 👋 916116514401316904
+
+//     Tech 727512898741600358
+//     MOST Points 780116637197926420
+//     Conquest 🗡️ 799318574077444107
+
+//     Prospect 811689644131483648
+//     Money 💰 725531758744961114
+//     Tech Spam Victims 1020747862960775188
+//     Member 724394130465357915
+//     Commander 👑 727311131705868299
+//     Academy 733739784643674253
+//     Art Event 916478169949818941
+
+//     New Coop Day 1029396511789953075
+//     Beginner 725531274973937664
+//     Projects👷 799318707246727188
+//     Member OF WEEK 725406725615779922
+
+//     Intermediate 726394740412776450
+
+//     Art Spam Victims 1020749616196616273
+//     MOST Items 821922830613479424
+//     Master 725531525398921335
+//  -->
+//     <!-- Cooper 799699580228861962 -->
+//     <!-- Agency 733739458855174265 -->
 }
