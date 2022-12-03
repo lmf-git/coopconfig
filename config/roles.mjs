e6e92956420c6fbc8c5424036f096e3a9f9a42e9
L M Fielding <contact@lmfielding.com>
2022-12-03 04:00:47 +0000

    Add user roles service

--- config/roles.mjs
+++ config/roles.mjs
@@ -2,138 +2,164 @@ export default {
     "MEMBER": {
         "name": "Member 🐔",
         "id": "724394130465357915",
-        "category": "DEMOCRACY"
+        "category": "DEMOCRACY",
+        "description": "Testing description"
     },
     "COMMANDER": {
         "name": "Commander 👑",
         "id": "727311131705868299",
-        "category": "DEMOCRACY"
+        "category": "DEMOCRACY",
+        "description": ""
     },
     "LEADER": {
         "name": "Leader ⚔️",
         "id": "723676356818239773",
-        "category": "DEMOCRACY"
+        "category": "DEMOCRACY",
+        "description": ""
     },
     "PROSPECT": {
         "name": "Prospect",
         "id": "811689644131483648",
-        "category": "DEMOCRACY"
+        "category": "DEMOCRACY",
+        "description": ""
     },
 
     "BEGINNER": {
         "name": "Beginner 🥚",
         "id": "725531274973937664",
-        "category": "REWARD"
+        "category": "REWARD",
+        "description": ""
     },
     "SUBSCRIBER": {
         "name": "Announcements",
         "id": "726835640804704267",
-        "category": "NOTIFICATION"
+        "category": "NOTIFICATION",
+        "description": ""
     },
     "MOSTPOINTS": {
         "name": "Most Points",
         "id": "780116637197926420",
-        "category": "REWARD"
+        "category": "REWARD",
+        "description": ""
     },
     "MEMBEROFWEEK": {
         "name": "Member Of Week",
         "id": "725406725615779922",
-        "category": "REWARD"
+        "category": "REWARD",
+        "description": ""
     },
     // "FORMER_COMMANDER": {
     //     "name": "Former Commander",
     //     "id": "799113973282111541",
-    //     "category": "REWARD"
+    //     "category": "REWARD",
+    // "description": ""
     // },
     "RICHEST": {
         "name": "RICHEST",
         "id": "821922924138463232",
-        "category": "REWARD"
+        "category": "REWARD",
+        "description": ""
     },
     "MOST_ITEMS": {
         "name": "MOST_ITEMS",
         "id": "821922830613479424",
-        "category": "REWARD"
+        "category": "REWARD",
+        "description": ""
     },
     "SUPPORTER": {
         "name": "SUPPORTER",
         "id": "904477558928715776",
-        "category": "REWARD"
+        "category": "REWARD",
+        "description": ""
     },
 
     "PROJECTS": {
         "name": "Projects",
         "id": "799318707246727188",
-        "category": "ACCESS"
+        "category": "ACCESS",
+        "description": ""
     },
     "SOCIAL": {
         "name": "Social",
         "id": "799419324485599242",
-        "category": "ACCESS"
+        "category": "ACCESS",
+        "description": ""
     },
     "LOGS": {
         "name": "Logs",
         "id": "796495195930886155",
-        "category": "ACCESS"
+        "category": "ACCESS",
+        "description": ""
     },
 
     "TECH": {
         "name": "Tech",
         "id": "727512898741600358",
-        "category": "INTEREST"
+        "category": "INTEREST",
+        "description": ""
     },
     "MONEY": {
         "name": "Money",
         "id": "725531758744961114",
-        "category": "INTEREST"
+        "category": "INTEREST",
+        "description": ""
     },
     "ART": {
         "name": "Art",
         "id": "727513157374967879",
-        "category": "INTEREST"
+        "category": "INTEREST",
+        "description": ""
     },
 
 
     "MINIGAME_PING": {
         "name": "MINIGAME_PING",
         "id": "916135701488209940",
-        "category": "NOTIFICATION"
+        "category": "NOTIFICATION",
+        "description": ""
     },
     "INTRO_POSTED_PING": {
         "name": "INTRO_POSTED_PING",
         "id": "916116514401316904",
-        "category": "NOTIFICATION"
+        "category": "NOTIFICATION",
+        "description": ""
     },
     "MARKET_OPEN_PING": {
         "name": "MARKET_OPEN_PING",
         "id": "1004584945202765844",
-        "category": "MONEY"
+        "category": "MONEY",
+        "description": ""
     },
     "NEW_COOP_DAY": {
         "name": "NEW_COOP_DAY",
         "id": "1029396511789953075",
-        "category": "NOTIFICATION"
+        "category": "NOTIFICATION",
+        "description": ""
     }
 
     // "CONQUEST": {
         // "name": "CONQUEST",
     //     "id": "799318574077444107",
-    //     "category": ""
+    //     "category": "",
+    // "description": ""
     // },
     // "GAMING": {
         // "name": "GAMING",
     //     "id": "811317877151760384",
-    //     "category": "INTEREST"
+    //     "category": "INTEREST",
+    // "description": ""
     // },
     // "AGENCY": {
         // "name": "AGENCY",
     //     "id": "733739458855174265",
-    //     "category": "INTEREST"
+    //     "category": "INTEREST",
+    // "description": ""
     // },
     // "ACADEMY": {
         // "name": "ACADEMY",
     //     "id": "733739784643674253",
-    //     "category": "INTEREST"
+    //     "category": "INTEREST",
+    // "description": ""
     // },
 
 
