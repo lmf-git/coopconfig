8225daba0ca8880ac266cb7f7d1823aa66c211ef
L M Fielding <contact@dreamsbyte.com>
2024-12-11 14:42:47 -0300

    Clean for restart.

--- helper/authHelper.mjs
+++ helper/authHelper.mjs
@@ -1,6 +1,5 @@
-import axios from 'axios';
+
 import jwt from 'jsonwebtoken';
-import { Strategy } from 'passport-jwt';
 
 import Users from '../services/users.mjs';
 
@@ -15,51 +14,6 @@ export default class Auth {
 	
 		return token;
 	};
-
-	static whoisMeViaDiscord = accessToken =>
-		axios.get('https://discord.com/api/users/@me', {
-			headers: { authorization: `Bearer ${accessToken}` }
-	});
-
-	static authorizeDiscord = (code) => 
-		axios.post('https://discord.com/api/oauth2/token', 
-			new URLSearchParams({
-				client_id: process.env.DISCORD_APPID,
-				client_secret: process.env.DISCORD_CLIENT_SECRET,
-				code,
-				grant_type: 'authorization_code',
-				redirect_uri:
-					process.env.NODE_ENV !== 'development' ?
-						`https://thecoop.group/auth/authorise`
-						:
-						`http://localhost:4500/auth/authorise`,
-				scope: 'identify'
-			}),
-			{ 
-				headers: {  'Content-Type': 'application/x-www-form-urlencoded' }
-			}
-		);
-
-	static strategy() {
-		const opts = {
-			jwtFromRequest: this.jwtFromRequest,
-			secretOrKey: process.env.DISCORD_TOKEN,
-			...this.issuerOpts
-		};
-		return new Strategy(opts, async (jwt_payload, done) => {		
-			try {
-				// Check user actually exists.
-				const user = await Users.get(jwt_payload.id);				
-				if (!user) 
-					throw new Error('Token does not represent a member of The Coop.');
-				
-				return done(null, user);
-
-			} catch(e) {
-				return done(e, false);
-			}
-		});
-	}
 
 	static issuerOpts = {
 		issuer: 'api.thecoop.group',
